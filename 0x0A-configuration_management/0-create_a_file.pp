3d61520b05511fd3c9ed92eb2cc071f4ab92d470
Ian Musima <sutomusima@gmail.com>
2023-03-27 07:01:52 +0000

    Fix checker issue

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,9 @@
-file {'/tmp/school':
-  path    => '/tmp/school',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet'
+node default {
+  file {'/tmp/school':
+    path    => '/tmp/school',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
+}
 }
