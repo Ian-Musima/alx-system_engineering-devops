39eaaea76722ac512f348a4c2f9f93958c04f046
Ian Musima <sutomusima@gmail.com>
2023-04-24 04:14:09 -0700

    Fix error, second attempt

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,11 +1,7 @@
 # create the file school
-
-node default {
-    file {'/tmp/school':
-        path => '/tmp/school',
-  mode       => '0744',
-  owner      => 'www-data',
-  group      => 'www-data',
-  content    => 'I love Puppet',
-}
+file {'/tmp/school':
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
